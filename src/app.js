0041b9bcb34b1724426a098f71e2227422c3b558
Kacian417 <knicho4492@gmail.com>
2024-09-19 01:04:52 +0000

    started project in class

--- src/app.js
+++ src/app.js
@@ -7,5 +7,41 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let firstWord = "The ";
+  // array of parts of sentence
+  let adjective = ["lonely ", "angry ", "slow ", "old ", "two-headed "];
+  let noun = ["jogger ", "dog ", "raccoon ", "grandma "];
+  let action = [
+    "threw my ",
+    "burned the ",
+    "kicked the ",
+    "stole the ",
+    "hid my ",
+    "destroyed my "
+  ];
+  let possession = ["luggage ", "biscuits ", "lamps ", "computer ", "phone "];
+  let location = [
+    "in the restrooom!",
+    "at the bar!",
+    "in the parking lot!",
+    "on the lawn!",
+    "at school!"
+  ];
+
+  //random selection of elements using Math.floor() and Math.random()
+  let random1 = Math.floor(Math.random() * adjective.length);
+  let random2 = Math.floor(Math.random() * noun.length);
+  let random3 = Math.floor(Math.random() * action.length);
+  let random4 = Math.floor(Math.random() * possession.length);
+  let random5 = Math.floor(Math.random() * location.length);
+
+  document.querySelector(".excuse").innerHTML =
+    firstWord +
+    adjective[random1] +
+    noun[random2] +
+    action[random3] +
+    possession[random4] +
+    location[random5];
 };
+
+// Bonus: create a function that will be called to generate a random number to be used in selction an element in each array
